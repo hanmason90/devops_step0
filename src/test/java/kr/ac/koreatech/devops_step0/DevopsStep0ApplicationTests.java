5e3aa96a04a7a9afaa7ff04e7064a46c76bc2688
HanMason <hanmason90@gmail.com>
2024-09-29 11:46:32 +0900

    Do Test Code  and Check Action

--- src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -1,5 +1,7 @@
 package kr.ac.koreatech.devops_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,23 @@ class DevopsStep0ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+// 두 수를 더하는 함수
+	int add(int a, int b){
+		return a + b;
+	}
+
+	@Test
+	void addTest1(){
+		int result = add(10, 20);
+		assertEquals(30, result);
+	}
+	
+	@Test
+	void addTest2(){
+		int result = add(10, 20);
+		assertEquals(10, result);
 	}
 
 }
